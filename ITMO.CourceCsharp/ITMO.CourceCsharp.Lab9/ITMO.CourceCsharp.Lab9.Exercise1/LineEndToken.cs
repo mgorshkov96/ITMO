fdf0c2f62f69b20476ecebe480958312f656294d
Maksim Gorshkov <m.gorshkov96@gmail.com>
2022-11-08 17:00:32 +0300

    I completed lab 9 exercise 1.

---
+++ ITMO.CourceCsharp/ITMO.CourceCsharp.Lab9/ITMO.CourceCsharp.Lab9.Exercise1/LineEndToken.cs
@@ -0,0 +1,26 @@
+namespace ITMO.CourceCsharp.Lab9.Exercise1
+{    
+    internal sealed class LineEnd
+    {
+        internal sealed class Token : ILineEndToken
+        {
+            internal Token(int number)
+            {
+                this.number = number;
+            }
+         
+            public static Token operator++(Token t)
+            {
+                t.number++;
+                return t;
+            }
+            
+            int ILineEndToken.Number()
+            {
+                return number; 
+            }
+            
+            private int number;   
+        }
+    }
+}
---
+++ ITMO.CourceCsharp/ITMO.CourceCsharp.Lab9/ITMO.CourceCsharp.Lab9.Exercise1/Main.cs
@@ -0,0 +1,26 @@
+namespace ITMO.CourceCsharp.Lab9.Exercise1
+{
+    using System;
+    using System.Data;
+
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            try {
+                InnerMain(args);
+            }
+            catch (System.Exception caught) {
+                Console.WriteLine(caught);
+            }
+        }
+        
+        public static void InnerMain(string[] args)
+        {
+            SourceFile source = new SourceFile(args[0]);
+            
+            HTMLTokenVisitor visitor = new HTMLTokenVisitor();
+            source.Accept(visitor);
+        }
+    }
+}
