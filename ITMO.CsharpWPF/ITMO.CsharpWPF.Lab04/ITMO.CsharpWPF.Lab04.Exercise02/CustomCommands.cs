ddcedff4944562ad72f64cfdabb63207cd2e797a
Maksim Gorshkov <m.gorshkov96@gmail.com>
2022-12-24 15:49:56 +0300

    CharpWPF Lab04 completed

---
+++ ITMO.CsharpWPF/ITMO.CsharpWPF.Lab04/ITMO.CsharpWPF.Lab04.Exercise02/CustomCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ITMO.CsharpWPF.Lab04.Exercise02
+{
+    public class CustomCommands
+    {
+        static CustomCommands()
+        {
+            // новая коллекция
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+
+            // добавляет соответствующую клавишу ввода, связанную с новой командой
+            // (Ctrl + L)
+            myInputGestures.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+
+            // инициирует переменную, возвращающую настраиваемую команду
+            Launch = new RoutedUICommand("Запуск", "Launch", typeof(CustomCommands), myInputGestures);
+        }
+
+        public static RoutedUICommand Launch { get; }
+    }
+}
