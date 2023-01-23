9fbe6d01627adf9179a117459197fa4e5c052495
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-01-23 19:52:42 +0300

    ITMO.ADONET.Lab05.Exercise03 completed

---
+++ ITMO.ADONET/ITMO.ADONET.Lab05/ITMO.ADONET.Lab05.Exercise03/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.ADONET.Lab05.Exercise03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BindGridButton_Click(object sender, EventArgs e)
+        {
+            // Заполните таблицу Products данными из базы данных
+            productsTableAdapter1.Fill(northwindDataSet1.Products);
+
+            // Создайте новый компонент BindingSource для таблицы Products
+            BindingSource productsBindingSource = new
+                BindingSource(northwindDataSet1, "Products");
+
+            // Свяжите сетку с компонентом BindingSource
+            ProductsGrid.DataSource = productsBindingSource;
+
+            // Свяжите навигатор с компонентом BindingSource
+            bindingNavigator1.BindingSource = productsBindingSource;
+        }
+    }
+}
