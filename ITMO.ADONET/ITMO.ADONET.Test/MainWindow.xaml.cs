852dbe9aa37e5aaaf0090c3e306f5e75869b2e42
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-01-29 21:50:00 +0300

    MainWindow completed

--- ITMO.ADONET/ITMO.ADONET.Test/MainWindow.xaml.cs
+++ ITMO.ADONET/ITMO.ADONET.Test/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace ITMO.ADONET.Test
         private DataSet NorthwindDataset = new DataSet("Northwind");
 
         private DataTable CustomersTable = new DataTable("Customers");
-        private DataTable OrdersTable = new DataTable("Orders");
+        private DataTable OrdersTable = new DataTable("Orders");        
 
         private DataView CustomersDataView;
         private DataView OrdersDataView;
@@ -47,13 +47,15 @@ namespace ITMO.ADONET.Test
         public void CreateDataset()
         {
             try
-            {
+            {                
                 CustomersAdapter = new SqlDataAdapter("SELECT * FROM Customers", NorthwindConnection);
                 OrdersAdapter = new SqlDataAdapter("SELECT * FROM Orders", NorthwindConnection);
-                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(CustomersAdapter);
+
+                SqlCommandBuilder customersCommandBuilder = new SqlCommandBuilder(CustomersAdapter);
+                SqlCommandBuilder ordersCommandBuilder = new SqlCommandBuilder(OrdersAdapter);
 
                 NorthwindDataset.Tables.Add(CustomersTable);
-                NorthwindDataset.Tables.Add(OrdersTable);                
+                NorthwindDataset.Tables.Add(OrdersTable);
 
                 CustomersAdapter.Fill(NorthwindDataset.Tables["Customers"]);
                 OrdersAdapter.Fill(NorthwindDataset.Tables["Orders"]);
@@ -64,6 +66,7 @@ namespace ITMO.ADONET.Test
                 if (NorthwindDataset.Tables["Customers"].Rows.Count > 0)
                 {
                     CustomersDataGrid.ItemsSource = CustomersDataView;
+                    CustomersDataGrid.SelectionMode = DataGridSelectionMode.Single;
                 }
 
                 if (NorthwindDataset.Tables["Orders"].Rows.Count > 0)
@@ -71,9 +74,7 @@ namespace ITMO.ADONET.Test
                     OrdersDataGrid.ItemsSource = OrdersDataView;
                     OrdersDataView.RowFilter = "CustomerID=''";
                 }
-
-                CustomersDataGrid.SelectionMode = DataGridSelectionMode.Single;                
-            }
+            }            
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
@@ -88,15 +89,7 @@ namespace ITMO.ADONET.Test
                 if (selectedRow != null)
                 {
                     string selectedID = selectedRow["CustomerID"].ToString();
-
-                    OrdersDataView.RowFilter = "CustomerID='" + selectedID + "'";
-
-                    //int selectedСolumn = CustomersDataGrid.CurrentCell.Column.DisplayIndex;
-                    //var selectedCell = CustomersDataGrid.SelectedCells[selectedСolumn];
-                    //var cellContent = selectedCell.Column.GetCellContent(selectedCell.Item);
-
-                    //currentCellTextBox.Text = (cellContent as TextBlock).Text;
-                    //statusCellTextBox.Text = selectedRow.RowVersion.ToString();
+                    OrdersDataView.RowFilter = "CustomerID='" + selectedID + "'";                    
                 }
                 else
                 {
@@ -110,25 +103,19 @@ namespace ITMO.ADONET.Test
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-            }
-
-            //MessageBox.Show(CustomersDataGrid.SelectedCells[1].Item.ToString());
-
-
+            }           
         }
 
-        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        private void SynchItem_Click(object sender, RoutedEventArgs e)
         {
             try
             {                
-                CustomersAdapter.Update(NorthwindDataset, "Customers");
-
-                //CustomersTable.Clear();
-                //NorthwindDataset.Clear(NorthwindDataset.Tables["Customers"]);
-                NorthwindDataset.Tables["Customers"].Clear();
-                
+                CustomersAdapter.Update(NorthwindDataset.Tables["Customers"]);
+                OrdersAdapter.Update(NorthwindDataset.Tables["Orders"]);
+                NorthwindDataset.Clear();                
                 CustomersAdapter.Fill(NorthwindDataset.Tables["Customers"]);
-                MessageBox.Show("Данные были синхронизированны");
+                OrdersAdapter.Fill(NorthwindDataset.Tables["Orders"]);
+                MessageBox.Show("Данные были синхронизированны");                
             }
             catch(Exception ex)
             {
@@ -138,38 +125,106 @@ namespace ITMO.ADONET.Test
 
         private void SortTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            string test = "";
-            if (SortTextBox.Text != test)
+            switch (SortTextBox.Text)
             {
-                CustomersDataView.Sort = "CustomerID='" + SortTextBox.Text + "'";
+                case "ID":
+                    CustomersDataView.Sort = "CustomerID";
+                    break;
+                case "Компания":
+                    CustomersDataView.Sort = "CompanyName";
+                    break;
+                case "Имя":
+                    CustomersDataView.Sort = "ContactName";
+                    break;
+                case "Должность":
+                    CustomersDataView.Sort = "ContactTitle";
+                    break;
+                case "Телефон":
+                    CustomersDataView.Sort = "Phone";
+                    break;
+                default:
+                    CustomersDataView.Sort = "";
+                    break;
             }
         }
 
         private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            string test = "";
-            try
+        {                       
+            if (!String.IsNullOrEmpty(FilterTextBox.Text))
             {
-                if (FilterTextBox.Text != test)
-                {
-                    CustomersDataView.RowFilter = "ContactTitle ='" + FilterTextBox.Text + "'";
-                }
-                else
-                {
-                    CustomersDataView.RowFilter = "";
-                }
+                CustomersDataView.RowFilter = "ContactTitle ='" + FilterTextBox.Text + "'";
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.Message);
-            }
-                                    
+                CustomersDataView.RowFilter = "";
+            }                                               
         }
 
-        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        private void customersDeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            DataRowView selectedRow = (DataRowView)CustomersDataGrid.SelectedItem;
-            selectedRow.Delete();
+            try
+            {
+                DataRowView selectedRow = (DataRowView)CustomersDataGrid.SelectedItem;
+                selectedRow.Delete();
+            }
+            catch (InvalidCastException)
+            {
+                return;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Сначала выберите строку");
+            }
+        }
+
+        private void ordersDeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataRowView selectedRow = (DataRowView)OrdersDataGrid.SelectedItem;
+                selectedRow.Delete();
+            }
+            catch (InvalidCastException)
+            {
+                return;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Сначала выберите строку");
+            }
+        }
+
+        private void customersAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView customersRowView = CustomersDataView.AddNew();            
+            customersRowView["CustomerID"] = "ZTEST";
+            customersRowView["CompanyName"] = "MyCompany";
+            customersRowView["ContactName"] = "Vasiliy";
+            customersRowView["Phone"] = "88005553535";
+            customersRowView.EndEdit();
+        }
+
+        private void ordersAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {                
+                DataRowView selectedRow = (DataRowView)CustomersDataGrid.SelectedItem;
+                string selectedID = selectedRow["CustomerID"].ToString();
+
+                DataRowView ordersRowView = OrdersDataView.AddNew();
+                ordersRowView["OrderID"] = 11078;
+                ordersRowView["CustomerID"] = selectedID;                
+                ordersRowView["ShipAddress"] = "Невский проспект, д. 1";
+                ordersRowView.EndEdit();                
+            }
+            catch (InvalidCastException)
+            {
+                return;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);                 
+            }                      
         }
     }
 }
