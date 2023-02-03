e9d20c8aa5d3847ffdbbeb4e0985e984cb9957ae
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-02-03 19:42:13 +0300

    ITMO.ASPNET.WebForms Lab01.Ex.01 completed

---
+++ ITMO.ADONET/ITMO.ADONET.Lab09/ITMO.ADONET.Lab09.Exercise01/ITMO.ADONET.Lab09.Exercise01.CodeFirst/SampleContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ITMO.ADONET.Lab09.Exercise01.CodeFirst
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext() : base("MyShop") 
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().Property(c => c.LastName).IsRequired().HasMaxLength(30);
+        }
+    }
+}
---
+++ ITMO.ASPNET/ITMO.ASPNET.WebForms/ITMO.ASPNET.WebForms.RSVP/App_Code/ResponseRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASPNET.WebForms.RSVP.App_Code
+{
+    public class ResponseRepository
+    {
+        private static ResponseRepository repository= new ResponseRepository();
+        private List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static ResponseRepository GetResponse()
+        {
+            return repository;
+        }
+
+        public IEnumerable<GuestResponse> GetAllResponses()
+        {
+            return responses;
+        }
+
+        public void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
