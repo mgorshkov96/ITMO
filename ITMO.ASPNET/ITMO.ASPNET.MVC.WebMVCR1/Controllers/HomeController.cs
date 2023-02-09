4389cf9eb8f33e8943fdf222d8e1dc85d9eb3d88
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-02-09 23:22:15 +0300

    ITMO.ASPNET.MVC Lab01 completed

---
+++ ITMO.ASPNET/ITMO.ASPNET.MVC.WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using ITMO.ASPNET.MVC.WebMVCR1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITMO.ASPNET.MVC.WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index(string hel)
+        {            
+            string Greeting = ModelClass.ModelHello() + ", " + hel;
+            return Greeting;
+        }
+    }
+}
